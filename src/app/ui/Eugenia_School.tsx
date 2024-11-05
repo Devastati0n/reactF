c8bd1a6cddd2132612f915cc366ec04bb142dc0a
Devastati0n <havokcs1@yahoo.com>
2024-11-04 21:22:45 -0800

    first

---
+++ src/app/ui/Eugenia_School.tsx
@@ -0,0 +1,36 @@
+
+import Image from 'next/image';
+import EugeniaSG from '../public/images/Eugenia-Cooney.jpg';
+
+
+export default function Eugenia() {
+  return (
+   <Image src={EugeniaSG} alt="Eugenia School Girl Outfit" width={35} height={35} />
+  );
+}
+
+
+
+
+/*
+export default function EugeniaSchool() {
+  return (
+   
+    <Image src="/public/Eugenia.jpg" alt="My Image" width={500} height={300} />
+  
+  );
+}
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
